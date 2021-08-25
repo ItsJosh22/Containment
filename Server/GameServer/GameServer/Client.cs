7633a7e6fac4f0a4de63a71d4f5f52e175cdf014
Joshua <jemartin1@student.fullsail.edu>
2021-08-24 21:35:28 -0400

    Added Server and base server

---
+++ Server/GameServer/GameServer/Client.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+        public int id;
+        public TCP tcp;
+
+        public Client(int _clientID)
+        {
+            id = _clientID;
+            tcp = new TCP(id);
+        }
+
+
+
+        public class TCP
+        {
+            public TcpClient socket;
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] recieveBuffer;
+
+             public TCP(int _id)
+            {
+                id = _id;
+            }
+
+
+            public void Connect (TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+
+                recieveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(recieveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+
+            }
+
+            private void ReceiveCallback(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLength = stream.EndRead(_result);
+                    if (_byteLength <= 0)
+                    {
+                        return;
+                    }
+
+                    byte[] _data = new byte[_byteLength];
+                    Array.Copy(recieveBuffer, _data, _byteLength);
+
+                    stream.BeginRead(recieveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                }
+                catch (Exception _ex)
+                {
+                    Console.WriteLine($"Error receiving TCP data: {_ex}");
+                    throw;
+                }
+            }
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
